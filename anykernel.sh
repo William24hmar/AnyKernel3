36d1ef67cae261316fef25142263d402da15d8cb
Willay <williamhmar24@gmail.com>
2026-07-18 23:53:01 +0530

    Remove permission settings for module files
    Removed permission setting for modules.load.recovery and modules.dep.

--- anykernel.sh
+++ anykernel.sh
@@ -1,5 +1,6 @@
 ### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
+
 ### AnyKernel setup
 # begin properties
 properties() { '
@@ -13,21 +14,21 @@ supported.versions=11.0-17.0
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
+
 # shell variables
 block=boot;
 is_slot_device=auto;
 ramdisk_compression=auto;
 patch_vbmeta_flag=auto;
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
+
 ## AnyKernel boot install
 dump_boot;
-vbmeta_disable_verification;
 
-## Ensure pushed replacement files keep correct ownership/permissions
-set_perm 0 0 644 lib/modules/modules.load.recovery;
-set_perm 0 0 644 lib/modules/modules.dep;
+vbmeta_disable_verification;
 
 write_boot;
 ## end boot install
